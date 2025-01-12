9d5342b003e4ba7faa4ddf1d4084032d7127e26d
Vedant Jayale <145029283+vedant-jayale@users.noreply.github.com>
2025-01-13 01:38:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -74,9 +74,12 @@ def get_prices():
                f"&Tx_CommodityHead={commodity_name}&Tx_StateHead={state_name}&Tx_DistrictHead={district_name}&Tx_MarketHead={market_name}")
 
         headers = {
+            
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
             "Accept-Language": "en-US,en;q=0.9",
-            "Referer": "https://www.agmarknet.gov.in"
+            "Referer": "https://agmarknet.gov.in",
+            "Connection": "keep-alive"
+
         }
 
         try:
